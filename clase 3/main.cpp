b2040248c26600fd15e2f1cf5cd5e5fd98f69e33
Unknown <davidestiven17@gmail.com>
2018-02-20 13:03:05 -0500

    primer ejercicio

--- clase 3/main.cpp
+++ clase 3/main.cpp
@@ -4,36 +4,42 @@ using namespace std;
 
 int main()
 {
-  int num , v ;
-  float total;
+    int num, v ;
+    float total;
 
-  cout << "escriba un numero del 1 al 4 para determinar el caso: " << endl;
-  cout << " 1 para exprecion = 100*n" << endl;
-  cout<< " 2 para exprecion = 100-n" << endl;
-  cout<< " 3 para  exprecion = 100/n" << endl;
+    cout << "escriba un numero del 1 al 4 para determinar el caso: " << endl;
+    cout << " 1 para exprecion = 100*n" << endl;
+    cout<< " 2 para exprecion = 100-n" << endl;
+    cout<< " 3 para  exprecion = 100/n" << endl;
     cout << " : ";
     cin >> num;
-  cout <<"ingrese el valor para ingresar en la operacion : ";
+    cout <<"ingrese el valor para ingresar en la operacion : ";
     cin >> v;
- cout << " " << endl;
-    switch (num)  {
+    cout << " " << endl;
+    switch (num)
+    {
 
     case  1 :
-     total == (100*v );
-     break ;
+        total == (100*v );
+        cout << "el resultato de la operacion  " << num << "  es : " << total<<endl;
+        break ;
     case 2 :
         total == (100-v);
+        cout << "el resultato de la operacion  " << num << "  es : " << total<<endl;
         break;
     case 3 :
         total== ( 100/v);
+        cout << "el resultato de la operacion  " << num << "  es : " << total<<endl;
         break;
-    case default ;
-      cout << "eres un bruto ese numero no corresponde a ninguna operacion ";
-    break;
+    default :
+        cout << "eres un bruto ese numero no corresponde a ninguna operacion ";
+        break;
 
     }
 
-    cout << "el resultato de la operacion  " << num << "  es : " << total;
+
+    cout << "   "<<endl;
+    cout << "   "<<endl;
 
 
     return 0;
